708c51ad33e47cfde82a8ec29faedf6d2d71be4c
repo-docs-local <debayanthenroy@gmail.com>
2025-08-20 16:18:14 +0530

    First Commit

---
+++ src/main/java/com/example/spring_aop_logback/SpringAopLogbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_aop_logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopLogbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopLogbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_aop_logback/SpringAopLogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_aop_logback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopLogbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
